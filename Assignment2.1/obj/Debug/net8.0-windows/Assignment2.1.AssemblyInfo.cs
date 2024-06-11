cd3fe609d7c91dbdb64cdf3860960829df9632de
cassandra <darcia2005.cb@icloud.com>
2024-06-11 14:51:59 -0400

    start

--- Assignment2.1/obj/Debug/net8.0-windows/Assignment2.1.AssemblyInfo.cs
+++ Assignment2.1/obj/Debug/net8.0-windows/Assignment2.1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("NewApp")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Assignment2.1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f584dcf4e75a6fb2cc0f36fc17e9f4896b3e296f")]
-[assembly: System.Reflection.AssemblyProductAttribute("NewApp")]
-[assembly: System.Reflection.AssemblyTitleAttribute("NewApp")]
+[assembly: System.Reflection.AssemblyProductAttribute("Assignment2.1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Assignment2.1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
